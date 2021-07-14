aba25f1a36ecf7d500abd9f18b0c240f470b6ea6
Figaric <59481330+Figaric@users.noreply.github.com>
2021-07-14 22:33:28 +0300

    Subtle changes to the index file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,22 @@
+import "reflect-metadata";
+import dotenv from "dotenv";
+dotenv.config();
+
+import express = require("express");
+import session = require("express-session");
 import connectRedis from "connect-redis";
 import redis from "ioredis";
-import env from "dotenv";
-import express from "express";
-import session from "express-session";
-env.config();
-
-import { __prod__ } from "./constants";
-import mongoose from "mongoose";
-import accountRouter from "./routes/AccountRouter";
+import { mongoose } from "@typegoose/typegoose";
+import userRouter from "./resolvers/UserRouter";
+import IsUserInputNull from "./middlewares/IsUserInputNull";
 
 const main = async () => {
     const app = express();
-    const RedisStore = connectRedis(session);
-    const redisClient = new redis(6379, "127.0.0.1");
 
-    mongoose.connect(process.env.CONNECTION_URL!, {
+    const RedisStore = connectRedis(session);
+    const redisClient = new redis();
+
+    mongoose.connect(process.env.INVADER_DB_URL!, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
@@ -27,23 +29,25 @@ const main = async () => {
             client: redisClient,
             disableTouch: true
         }),
-        saveUninitialized: false,
-        resave: false,
-        secret: process.env.SESSION_SECRET!,
         cookie: {
             httpOnly: true,
-            maxAge: 1000 * 60 * 60,
+            maxAge: 1000 * 60 * 60 * 24 * 365 * 5,
             sameSite: "lax",
-            secure: __prod__
-        }
+            secure: process.env.NODE_ENV === "production"
+        },
+        resave: false,
+        saveUninitialized: false,
+        secret: process.env.SESSION_SECRET!
     }));
 
-    // routes
-    app.use("/account", accountRouter);
+    // routers
+    app.use("/account", IsUserInputNull, userRouter);
 
     app.listen(8080, () => {
         console.log("Server started on localhost:8080");
     });
 };
 
-main();
+main().catch(err => {
+    console.error(err);
+});
