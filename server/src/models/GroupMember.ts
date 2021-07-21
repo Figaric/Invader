7d26f9ae95d51e2a8dc65969e18d596006fadbdd
Figaric <59481330+Figaric@users.noreply.github.com>
2021-07-21 18:49:48 +0300

    Make GroupMember entity a graphql object

--- server/src/models/GroupMember.ts
+++ server/src/models/GroupMember.ts
@@ -1,19 +1,24 @@
+import { Field, Int, ObjectType } from "type-graphql";
 import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
 import Group from "./Group";
 import User from "./User";
 
 @Entity({ name: "groupMembers" })
+@ObjectType()
 export default class GroupMember extends BaseEntity {
     @Column({ default: false })
+    @Field()
     isAdmin: boolean;
 
     @PrimaryColumn()
+    @Field(() => Int)
     memberId: number;
     @ManyToOne(() => User, u => u.groups)
     @JoinColumn({ name: "memberId" })
     member: User;
     
     @PrimaryColumn()
+    @Field(() => Int)
     groupId: number;
     @ManyToOne(() => Group, g => g.members)
     @JoinColumn({ name: "groupId" })
