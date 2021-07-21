44a68ec8d25ed80b775321931a873b3dcad2cba6
Figaric <59481330+Figaric@users.noreply.github.com>
2021-07-21 18:46:30 +0300

    Make Post entity a graphql object

--- server/src/models/Post.ts
+++ server/src/models/Post.ts
@@ -1,31 +1,39 @@
+import { Field, Int, ObjectType } from "type-graphql";
 import { BaseEntity, Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import Group from "./Group";
 import User from "./User";
 
 @Entity({ name: "posts" })
+@ObjectType()
 export default class Post extends BaseEntity {
     @PrimaryGeneratedColumn()
+    @Field(() => Int)
     id: number;
 
     @Column({ nullable: false, length: 255 })
+    @Field()
     title!: string;
 
     @Column({ nullable: false })
+    @Field()
     text!: string;
 
     @Column()
+    @Field(() => Int)
     authorId!: number;
     @ManyToOne(() => User, u => u.posts)
     @JoinColumn({ name: "authorId" })
     author: User;
 
     @Column()
+    @Field(() => Int)
     groupId!: number;
     @ManyToOne(() => Group, g => g.posts)
     @JoinColumn({ name: "groupId" })
     group: Group;
 
     @CreateDateColumn()
+    @Field()
     createdAt: Date;
 
     @UpdateDateColumn()
