ee15131c514f5322c7fdfbfffbdd3f0a51e794f1
Figaric <59481330+Figaric@users.noreply.github.com>
2021-07-21 18:46:11 +0300

    Add getGroupMembers and getGroup queries

--- server/src/resolvers/GroupResolver.ts
+++ server/src/resolvers/GroupResolver.ts
@@ -8,6 +8,36 @@ import GroupMember from "../models/GroupMember";
 
 @Resolver(Group)
 export default class GroupResolver {
+
+    @Query(() => [GroupMember])
+    async getGroupMembers(
+        @Arg("groupId") groupId: number
+    ): Promise<GroupMember[]> {
+        const members = await getConnection().query(`
+        select gm.isAdmin,
+        json_build_object(
+            'id', u.id
+            'username', u.username
+        ) member
+        from "groupMembers" gm
+        inner join public.users u on u.id = gm."memberId"
+        where gm."groupId" = $1;
+        `, [groupId]);
+
+        console.log(members);
+
+        return members;
+    }
+
+    @Query(() => Group, { nullable: true })
+    async getGroup(
+        @Arg("groupId") groupId: number
+    ): Promise<Group | undefined> {
+        const group = await Group.findOne({ where: { id: groupId } });
+
+        return group;
+    }
+
     @Query(() => [Group])
     @UseMiddleware(MustBeAuth)
     async myGroups(
@@ -35,11 +65,11 @@ export default class GroupResolver {
         const group = await Group.findOne({ where: { name: groupName } });
 
         if(!group) {
-            throw new FieldError("groupName", "Group with name " + groupName + " is not found.", "NOT_FOUND");
+            throw new FieldError("groupName", "Group with name " + groupName + " is not found.");
         }
 
         if(await GroupMember.findOne({ where: { groupId: group.id, memberId: req.session.userId } })) {
-            throw new FieldError("groupName", "You're already a member of this group.", "ALREADY_IN_GROUP");
+            throw new FieldError("groupName", "You're already a member of this group.");
         }
 
         await GroupMember.insert({
@@ -49,74 +79,25 @@ export default class GroupResolver {
 
         return true;
     }
+
+    @Mutation(() => Boolean)
+    @UseMiddleware(MustBeAuth)
+    async createGroup(
+        @Arg("groupName") groupName: string,
+        @Ctx() { req }: ContextType
+    ): Promise<boolean> {
+        await getConnection().transaction(async t => {
+            const newGroup = await t.query(`
+            insert into "groups" ("name") values ($1)
+            `, [groupName]);
+
+            console.log("New group: ", newGroup);
+
+            await t.query(`
+            insert into "groupMembers" ("userId", "groupId") values ($1, $2)
+            `, [req.session.userId, newGroup.id]);
+        });
+
+        return true;
+    }
 }
-
-// groupRouter.get("/my-groups", async (req: RequestType, res) => {
-//     const userId = req.session.userId;
-
-//     const myGroups = await getConnection().query(`
-//     select gm.*,
-//     json_build_object(
-//     'id', g.id,
-//     'name', g.name) "group"
-//     from "groupMembers" gm
-//     inner join "groups" g on g.id = gm."groupId"
-//     where gm."memberId" = $1;
-//     `, [userId]);
-
-//     return res.status(200).json({ ok: true, data: myGroups });
-// });
-
-// groupRouter.post("/join", async (req: RequestType, res) => {
-//     const { groupName } = req.body;
-
-//     if(isNullOrWhitespace(groupName)) {
-//         return res.status(400).send({ ok: false, error: { field: "groupName", message: "Group name is required." } });
-//     }
-
-//     const group = await Group.findOne({ where: { name: groupName } });
-
-//     if(!group) {
-//         return res.status(404).send({ ok: false, error: { field: "groupName", message: "Group with this name is not found." } });
-//     }
-
-//     if(await GroupMember.findOne({ where: { groupId: group.id, memberId: req.session.userId } })) {
-//         return res.status(404).send({ ok: false, error: { field: "groupName", message: "You're already in this group." } });
-//     }
-
-//     const groupMember = GroupMember.create({
-//         groupId: group.id,
-//         memberId: req.session.userId
-//     });
-
-//     await GroupMember.save(groupMember);
-
-//     return res.status(200).json({ ok: true });
-// });
-
-// groupRouter.post("/", async (req: RequestType, res) => {
-//     const { groupName } = req.body;
-
-//     if(isNullOrWhitespace(groupName)) {
-//         return res.status(400).json({ ok: false, error: { field: "groupName", message: "GroupName is required" } });
-//     }
-
-//     await getConnection().transaction(async t => {
-        
-//         const group = Group.create({
-//             name: groupName
-//         });
-
-//         await t.save(group);
-
-//         await t.query(`
-        
-//         insert into "groupMembers" ("isAdmin", "groupId", "memberId") values (true, $1, $2);
-
-//         `, [group.id, req.session.userId]);
-//     });
-
-//     return res.status(201).json({ ok: true });
-// });
-
-// export default groupRouter;
