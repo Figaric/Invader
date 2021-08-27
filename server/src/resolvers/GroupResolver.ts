33785e617e3681d6bf5bbb2ed693fe217835fcb8
Tikhon Shesterikov <59481330+Figaric@users.noreply.github.com>
2021-08-27 21:39:32 +0300

    Add new resolvers to GroupResolver
    New queries:
    getGroupMembers
    MyGroups
    New mutations:
    LeaveGroup
    JoinGroup

--- server/src/resolvers/GroupResolver.ts
+++ server/src/resolvers/GroupResolver.ts
@@ -1,11 +1,49 @@
 import Group from "../entities/Group";
 import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
 import { getConnection } from "typeorm";
-import GroupMember from "src/entities/GroupMember";
-import ApolloContext from "src/types/ApolloContext";
+import GroupMember from "../entities/GroupMember";
+import ApolloContext from "../types/ApolloContext";
+import { UserInputError } from "apollo-server-core";
 
 @Resolver(Group)
 export default class GroupResolver {
+    @Query(() => [GroupMember])
+    async myGroups(
+        @Ctx() { req }: ApolloContext
+    ): Promise<GroupMember[]> {
+        const groups = await getConnection("development").query(`
+        select gm."isAdmin",
+        gm."groupId",
+        json_build_object(
+            'name', g.name,
+            'description', g.description
+        ) "group"
+        from "groupMembers" gm
+        inner join public.groups g on g.id = gm."groupId"
+        where gm."memberId" = $1;
+        `, [req.session.userId]);
+
+        return groups;
+    }
+
+    @Query(() => [GroupMember])
+    async getGroupMembers(
+        @Arg("groupId", () => Int) groupId: number
+    ): Promise<GroupMember[]> {
+        const groupMembers = await getConnection("development").query(`
+        select gm."isAdmin",
+        gm."memberId",
+        json_build_object(
+            'username', u.username
+        ) member
+        from "groupMembers" gm
+        inner join public.users u on u.id = gm."memberId"
+        where gm."groupId" = $1;
+        `, [groupId]);
+
+        return groupMembers;
+    }
+
     @Query(() => Group)
     async getGroup(
         @Arg("groupId", () => Int) groupId: number
@@ -19,9 +57,52 @@ export default class GroupResolver {
         return group;
     }
 
+    @Mutation(() => Boolean)
+    async leaveGroup(
+        @Arg("groupId", () => Int) groupId: number,
+        @Ctx() { req }: ApolloContext
+    ): Promise<Boolean> {
+        try {
+            await GroupMember.delete({
+                groupId,
+                memberId: req.session.userId
+            });
+        } catch {
+            if(!(await GroupMember.findOne({ where: { groupId, memberId: req.session.userId } }))) {
+                throw new UserInputError("You're not a member of this group", {
+                    field: "groupId"
+                });
+            }
+        }
+        
+        return true;
+    }
+
+    @Mutation(() => Boolean)
+    async joinGroup(
+        @Arg("groupId", () => Int) groupId: number,
+        @Ctx() { req }: ApolloContext
+    ): Promise<Boolean> {
+        try {
+            await GroupMember.insert({
+                groupId,
+                memberId: req.session.userId,
+                isAdmin: false
+            });
+        } catch {
+            if(await GroupMember.findOne({ where: { groupId, memberId: req.session.userId } })) {
+                throw new UserInputError("You're already in this group", {
+                    field: "groupId"
+                });
+            }
+        }
+
+        return true;
+    }
+
     @Mutation(() => Int)
     async createGroup(
-        @Arg("name") groupName: string,
+        @Arg("name") name: string,
         @Arg("description") description: string,
         @Ctx() { req }: ApolloContext
     ): Promise<number> {
@@ -29,10 +110,19 @@ export default class GroupResolver {
         
         await getConnection("development").transaction(async t => {
             const newGroup = t.create<Group>(Group, {
-                name: groupName,
+                name,
                 description
             });
-            await t.insert(Group, newGroup);
+
+            try {
+                await t.insert(Group, newGroup);
+            } catch {
+                if(await Group.findOne({ where: { name } })) {
+                    throw new UserInputError("This name is already taken.", {
+                        field: "name"
+                    });
+                }
+            }
 
             newGroupId = newGroup.id;
 
