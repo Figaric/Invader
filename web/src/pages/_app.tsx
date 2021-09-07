e6efb2d4bcd9aefec099e6ea1e7f93c4d87f4437
Figaric <59481330+Figaric@users.noreply.github.com>
2021-09-07 15:27:57 +0300

    Extract apollo client to a separate file
    And add websocket connection

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,36 +2,19 @@ import { ChakraProvider } from '@chakra-ui/react'
 
 import theme from '../theme'
 import { AppProps } from 'next/app';
-import { WebSocketLink } from '@apollo/client/link/ws';
-import { ApolloClient, ApolloProvider, concat, HttpLink, InMemoryCache } from '@apollo/client';
 import React from 'react';
-
-const httpLink = new HttpLink({
-  credentials: "include",
-  uri: "http://localhost:8080/graphql"
-});
-
-const wsLink = process.browser ? new WebSocketLink({
-  uri: "ws://localhost:8080/graphql",
-  options: {
-    reconnect: true
-  }
-}) : null;
-
-const actualLink = wsLink ? concat(wsLink, httpLink) : httpLink;
-
-const apolloClient = new ApolloClient({
-  link: actualLink,
-  cache: new InMemoryCache()
-});
-
+import apolloClient from '../apollo';
+import { ApolloProvider } from '@apollo/client';
+import "../index.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ApolloProvider client={apolloClient}>
+      <ChakraProvider resetCSS theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ApolloProvider>
   )
 }
 
-export default MyApp
+export default MyApp;
