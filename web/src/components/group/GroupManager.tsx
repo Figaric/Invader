dbe406e03b13db30553cef7dc8912f64689b219b
Figaric <59481330+Figaric@users.noreply.github.com>
2021-09-07 15:34:00 +0300

    Add GroupManager component
    If the user is an admin in the group,
    display this component so that he can edit or delete group

--- web/src/components/group/GroupManager.tsx
+++ web/src/components/group/GroupManager.tsx
@@ -1,25 +1,62 @@
-import { Box, Button, Flex } from '@chakra-ui/react';
-import React from 'react';
+import { Box, Button, Flex, IconButton } from '@chakra-ui/react';
+import { DeleteIcon, EditIcon, SettingsIcon } from "@chakra-ui/icons"
+import React, { useState } from 'react';
+import { shallowShadow } from '../../theme';
 
-interface GroupManagerProps {
-    
-}
-
-const GroupManager: React.FC<GroupManagerProps> = ({ }) => {
+function GroupManagerDropdown() {
     return (
         <Flex
-            flexDir={"column"}>
+            position={"absolute"}
+            top={"195px"}
+            p={15}
+            borderWidth={"1px"}
+            borderColor={"darkGray"}
+            bg={"semiDark"}
+            translateX={25}
+            rounded={"1rem"}
+            flexDir={"column"}
+            experimental_spaceY={2}
+            boxShadow={shallowShadow}>
             <Button
-                colorScheme={"red"}
-                mb={4}>
+                colorScheme={"default"}
+                leftIcon={<EditIcon/>}>
+                Edit Group
+            </Button>
+            <Button 
+                colorScheme={"delete"} 
+                leftIcon={<DeleteIcon/>}>
                 Delete Group
             </Button>
-            <Button
-                colorScheme={"gray"}>
-                Edit Group Info
-            </Button>
         </Flex>
-    )
+    );
+}
+
+
+interface GroupManagerProps {
+    groupId: number;
+}
+
+const GroupManager: React.FC<GroupManagerProps> = ({ groupId }) => {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <Box>
+            {/* <Button
+                colorScheme={"orange"}
+                aria-label={"Open group settings"}
+                leftIcon={<SettingsIcon/>}
+                onClick={() => setOpen(!open)}>
+                Manage Group
+            </Button> */}
+            <IconButton
+                colorScheme={"orange"}
+                aria-label={"Open group settings"}
+                icon={<SettingsIcon/>}
+                onClick={() => setOpen(!open)} />
+                
+            {open && <GroupManagerDropdown />}
+        </Box>
+    );
 }
 
 export default GroupManager;
